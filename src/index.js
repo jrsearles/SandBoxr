a9b31b8baba5462706a93b7d30bd6d451a04ca30
Joshua Searles <jrsearles@comcast.net>
2015-07-23 20:41:20 -0400

    update for publish

---
+++ src/index.js
@@ -0,0 +1,34 @@
+var handlers = require("./handlers");
+var Environment = require("./env");
+var ExecutionContext = require("./execution-context");
+
+function SandBoxr (ast, config) {
+	this.ast = ast;
+	this.config = config || {};
+	this.env = null;
+}
+
+SandBoxr.prototype.execute = function (context) {
+	if (!this.env) {
+		// create environment if it hasn't been created
+		this.createEnvironment();
+		this.env.init(this.config);
+	}
+	
+	if (!context) {
+		// initial call - create initial context
+		context = new ExecutionContext(this.env, this.ast);
+	}
+
+	if (!(context.node.type in handlers)) {
+		throw new TypeError("No handler defined for: " + context.node.type);
+	}
+
+	return handlers[context.node.type](context);
+};
+
+SandBoxr.prototype.createEnvironment = function () {
+	return this.env = new Environment(this);
+};
+
+module.exports = SandBoxr;
